a86b7e30690807d3711dcd55dd08a059b14373be
Khubaib Bin Asad <khubaibbinasad786@gmail.com>
2023-01-04 13:19:02 +0500

    make doctor route, changes in server js

---
+++ routes/doctorRoute.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+const Doctor = require('../models/doctorModel');
+const authMiddleware = require("../middlewares/authMiddleware");
+
+
+
+router.post("/get-doctor-info-by-user-id", authMiddleware, async (req, res) => {
+    try {
+      const doctor = await Doctor.findOne({ userId: req.body.userId });
+      res.status(200).send({
+        success: true,
+        message :"Doctor info fetched successfully",
+        data : doctor
+      })
+    } catch (error) {
+      res
+        .status(500)
+        .send({
+          message: "Error While getting doctor info",
+          success: false,
+          error,
+        });
+    }
+  });
+
+
+  module.exports = router;
--- server.js
+++ server.js
@@ -4,10 +4,12 @@ require('dotenv').config()
 const dbConfig = require("./config/dbConfig");
 app.use(express.json());
 const userRoute = require('./routes/userRoute');
-const adminRoute = require('./routes/adminRoute')
+const adminRoute = require('./routes/adminRoute');
+const doctorRoute = require('./routes/doctorRoute');
 
 app.use('/api/user',userRoute);
 app.use('/api/admin',adminRoute);
+app.use('/api/doctor',doctorRoute);
 const port =  process.env.PORT || 5000;
 
 // console.log(process.env.MONGO_URL)
